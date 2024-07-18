fe256a52068f5001a1c0fd0f1db4621707d1b82c
Toyosi Olaribigbe <ot.olaribigbe@outlook.com>
2024-07-18 17:10:32 +0100

    adding nodemon to backend

--- backend/backend.js
+++ backend/backend.js
@@ -1,96 +1,3 @@
-// require('dotenv').config();
-// const express = require("express");
-// const plaid = require("plaid")
-// const app = express();
-// const PORT = 4090;
-
-// // Initialize the Plaid client
-// const client = new plaid.Client({
-//   clientID: functions.config().PLAID_CLIENT_ID,
-//   secret: functions.config().PLAID_SECRET,
-//   env: plaid.PlaidEnvironments.development,
-// });
-
-// console.log(client)
-
-// app.use(express.json());
-
-// //get account balances
-// app.post('/accounts_balance', async (req, res) => {
-//   try{
-//     const response = await client.getBalance(req.body.access_token).catch((err) => {
-//       // handle error
-//       console.log(err);
-//     });
-//     //const accounts = response.accounts;
-//     res.json({
-//       accounts: response.accounts
-//     });
-//   }
-//   catch(e){
-//     console.log(e)
-//   }
- 
-// });
-
-
-// //endpoint exchanges public token for an access token currently working on this part
-// app.post('/exchange_public_token', async (request, res, next) => {
-//   try{
-//   //public token cant be found
-//     const response = await client
-//     .exchangePublicToken(request.body.public_token)
-//     .catch((err) => {
-//       console.log(err)
-//     });
-//   const accessToken = response.access_token;
-//   const itemId = response.item_id;
-//   // ACCESS_TOKEN = response.access_token;
-//   // ITEM_ID = response.item_id;
-//   res.json({
-//     access_token: accessToken,
-//     item_id: itemId
-//   });
-//   console.log("access token below");
-//   console.log(accessToken);
-//   }
-//   catch(e){
-//     console.log(e)
-//   }
- 
-// });
-
-// //endpoint returns link_token 
-// app.post('/create_link_token', async (request, response, next) => {
-//   try{
-//     // 1. Grab the client_user_id by searching for the current user in your database
-//     const user = "testUser"
-//     const clientUserId = "123";
-//     // 2. Create a link_token for the given user
-//     const linkTokenResponse = await client.createLinkToken({
-//       user: {
-//         client_user_id: clientUserId,
-//       },
-//       client_name: 'My App',
-//       products: ['transactions'],
-//       country_codes: ['US'],
-//       language: 'en',
-//     });
-//     const link_token = linkTokenResponse.link_token;
-//     // 3. Send the data to the client
-//     response.json({ link_token });
-//   }
-//   catch(e){
-//     console.log(e)
-//   }
- 
-// });
-
-
-
-// app.listen(PORT, () => {
-// console.log(`Server running on ${PORT}`);
-// });
 
 'use strict';
 
@@ -621,7 +528,7 @@ app.use('/api', function (error, request, response, next) {
 });
 
 const server = app.listen(APP_PORT, function () {
-  console.log('plaid-quickstart server listening on port ' + APP_PORT);
+  console.log('Server listening on port ' + APP_PORT);
 });
 
 const prettyPrintResponse = (response) => {
