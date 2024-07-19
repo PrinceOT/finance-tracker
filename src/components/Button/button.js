1571dcc068adb7acf23462351c98efd7e91a6bf6
Toyosi Olaribigbe <ot.olaribigbe@outlook.com>
2024-07-19 02:23:04 +0100

    Updates

--- src/components/Button/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./button.css"
-function Button({disable,text,onClick,blu}) {
-  return (
-    <div className={blu ? "btn btn-blue" : "btn"} onClick={onClick} disabled={disable}>{text}</div>
-  )
-}
-
-export default Button
